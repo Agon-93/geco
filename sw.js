36637671c0b7c6beba3b76b97027b952d4c96948
Agon-93 <115547616+Agon-93@users.noreply.github.com>
2026-06-01 11:06:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'geco-v48';
+const CACHE = 'geco-v49';
 const ASSETS = [
     './index.html', './calendario.html', './note.html',
     './notifiche.html', './profili.html', './riproduzione.html',
